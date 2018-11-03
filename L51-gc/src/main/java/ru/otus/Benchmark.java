8b9a0d34b08edb9cc7a4a52ae38bbea132cf6600
Sergey Zhilin <s.zhilin@semrush.com>
2018-11-03 22:21:16 +0300

    Heap bench added

---
+++ L51-gc/src/main/java/ru/otus/Benchmark.java
@@ -0,0 +1,33 @@
+package ru.otus;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Benchmark {
+
+    public static void test() {
+        List<Cargo> list = new ArrayList<>();
+        Random rand = new Random();
+
+        while (true) {
+            int count = 1 * 1000 * 1000;
+
+            for (int i = 0; i < count; i++) {
+                list.add(new Cargo(rand.nextLong()));
+            }
+
+            System.out.println(
+                String.format(
+                    "Created %d local objects",
+                    count
+                )
+            );
+
+            Gc.state();
+            for (int i = list.size() - count/2; i < list.size(); i++) {
+                list.remove(i);
+            }
+        }
+    }
+}
